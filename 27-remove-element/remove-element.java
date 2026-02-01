dee3ed0a849a6aefda851bac1e82b3e703566d30
jain-ishita-45 <jain.ishita.721@gmail.com>
2026-02-01 11:26:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (54.51%) - LeetSync

---
+++ 27-remove-element/remove-element.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+       int ans[]=new int [nums.length];
+       int j=0;
+       for(int i=0;i<nums.length;i++)
+       {
+        if(nums[i]!=val)
+        {
+            ans[j]=nums[i];
+            j++;
+        }
+       } 
+       for(int i=0;i<j;i++)
+       {
+            nums[i]=ans[i];
+       }
+       int k=j;
+       for(int i=j;i<nums.length;i++)
+       nums[i]=0;
+       return k;
+    }
+}
