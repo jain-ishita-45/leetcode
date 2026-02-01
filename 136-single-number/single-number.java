7821ea3f9eb960193ce363ab01b2427b93384923
jain-ishita-45 <jain.ishita.721@gmail.com>
2026-02-01 11:19:54 +0530

    Time: 13 ms (19.90%) | Memory: 47.1 MB (40.00%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer>mp=new HashMap<>();
+        for(int i:nums)
+        mp.put(i,mp.getOrDefault(i,0)+1);
+        for(Map.Entry<Integer,Integer> e:mp.entrySet())
+        {
+            if(e.getValue()==1)
+            return e.getKey();
+        }
+        return 0;
+    }
+}
