342a28f517b36b7f21e27d3c4d222f16b1104063
jain-ishita-45 <jain.ishita.721@gmail.com>
2026-02-18 19:26:39 +0530

    Time: 17 ms (53.69%) | Memory: 47.3 MB (49.84%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,48 +1,42 @@
-import java.util.*;
-
 class Solution {
     public String minWindow(String s, String t) {
+
         if (s.length() < t.length()) return "";
 
-        Map<Character, Integer> targetFreq = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            targetFreq.put(c, targetFreq.getOrDefault(c, 0) + 1);
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
         }
 
-        int required = targetFreq.size();
-        int formed = 0;
-
-        Map<Character, Integer> windowCounts = new HashMap<>();
         int l = 0, r = 0;
-        int[] ans = {-1, 0, 0}; // length, left, right
+        int cnt = 0;
+        int minLen = Integer.MAX_VALUE;
+        int start = -1;
 
         while (r < s.length()) {
-            char c = s.charAt(r);
-            windowCounts.put(c, windowCounts.getOrDefault(c, 0) + 1);
+            char ch = s.charAt(r);
 
-            if (targetFreq.containsKey(c) && windowCounts.get(c).intValue() == targetFreq.get(c).intValue()) {
-                formed++;
+            if (map.containsKey(ch)) {
+                if (map.get(ch) > 0) cnt++;
+                map.put(ch, map.get(ch) - 1);
             }
 
-            // Try to shrink window from left
-            while (l <= r && formed == required) {
-                if (ans[0] == -1 || r - l + 1 < ans[0]) {
-                    ans[0] = r - l + 1;
-                    ans[1] = l;
-                    ans[2] = r;
+            while (cnt == t.length()) {
+                if (r - l + 1 < minLen) {
+                    minLen = r - l + 1;
+                    start = l;
                 }
 
-                char leftChar = s.charAt(l);
-                windowCounts.put(leftChar, windowCounts.get(leftChar) - 1);
-                if (targetFreq.containsKey(leftChar) && windowCounts.get(leftChar).intValue() < targetFreq.get(leftChar).intValue()) {
-                    formed--;
+                char left = s.charAt(l);
+                if (map.containsKey(left)) {
+                    map.put(left, map.get(left) + 1);
+                    if (map.get(left) > 0) cnt--;
                 }
-                l++;
+                l++;   
             }
             r++;
         }
 
-        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+        return start == -1 ? "" : s.substring(start, start + minLen);
     }
 }
-
