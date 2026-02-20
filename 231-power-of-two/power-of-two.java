8901dafd05d37410779154f3db67914efb78f418
jain-ishita-45 <jain.ishita.721@gmail.com>
2026-02-20 13:22:41 +0530

    Time: 1 ms (95.89%) | Memory: 42.8 MB (35.22%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        int c=0;
+        while(n>0)
+        {
+            if((n&1)==1)
+            c++;
+            n=n>>1;
+        }
+    return c==1;
+    }
+
+}
