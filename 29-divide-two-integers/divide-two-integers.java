50137b4b5ba8538f434db3fab784b77e512ed082
jain-ishita-45 <jain.ishita.721@gmail.com>
2026-02-22 21:35:37 +0530

    Time: 1 ms (90.03%) | Memory: 43 MB (12.25%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -1,26 +1,29 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        if(dividend==divisor)
-        return 1;
-        if(dividend==0)
-        return 0;
-        Boolean sign=true;
-        if((dividend>=0 && divisor<0)||(dividend<=0 && divisor>0))
-        sign=false;
-        long n=Math.abs((long)dividend), d=Math.abs((long)divisor);
-        long ans=0;
-        while(n>=d)
-        {
-            int c=0;
-            while(n>=(d<<(c+1)))
-            c++;
-            ans+=1<<c;
-            n=n-(d<<c);
+
+        // Only overflow case
+        if (dividend == Integer.MIN_VALUE && divisor == -1)
+            return Integer.MAX_VALUE;
+
+        if (divisor == 0)
+            return Integer.MAX_VALUE;
+
+        boolean sign = (dividend >= 0) == (divisor >= 0);
+
+        long n = Math.abs((long) dividend);
+        long d = Math.abs((long) divisor);
+
+        long ans = 0;
+
+        while (n >= d) {
+            int c = 0;
+            while (n >= (d << (c + 1))) {
+                c++;
+            }
+            ans += (1L << c);
+            n -= (d << c);
         }
-        if(ans==(1<<31) && sign ==true)
-        return Integer.MAX_VALUE;
-         if(ans==(1<<31) && sign ==false)
-        return Integer.MIN_VALUE;
-        return (sign==true)? (int)ans: (int)(-1*ans);
+
+        return sign ? (int) ans : (int) -ans;
     }
 }
