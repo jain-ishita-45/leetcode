b596ebf67b9661d82f443c906bb6aef3b22db2c9
jain-ishita-45 <jain.ishita.721@gmail.com>
2026-06-19 20:06:25 +0530

    Time: 2 ms (91.94%) | Memory: 47.3 MB (97.66%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int []n=new int[nums.length];
+        int j=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=0)
+            n[j++]=nums[i];
+        }
+        for(int i=0;i<nums.length;i++)
+        nums[i]=n[i];
+    }
+}
