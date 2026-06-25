4bf77cbccaa95ec8bb7196853adbde89d1828d6b
jain-ishita-45 <jain.ishita.721@gmail.com>
2026-06-25 19:41:15 +0530

    Time: 41 ms (5.15%) | Memory: 99.1 MB (38.76%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,38 +1,49 @@
-import java.util.*;
-
 class MinStack {
-    Stack<Integer> st;
-    Stack<Integer> minSt;
+    Stack<Integer> s1;
+    Stack<Integer> s2;
 
     public MinStack() {
-        st = new Stack<>();
-        minSt = new Stack<>();
+        s1=new Stack<>();
+        s2=new Stack<>();
+        
     }
     
-    public void push(int val) {
-        st.push(val);
+    public void push(int value) {
+        s1.push(value);
+        if(s2.isEmpty() || value<=s2.peek())
+        s2.push(value);
         
-        if(minSt.isEmpty() || val <= minSt.peek())
-            minSt.push(val);
     }
     
     public void pop() {
-        if(!st.isEmpty()) {
-            int x = st.pop();
-            if(x == minSt.peek())
-                minSt.pop();
-        }
+        int x=0;
+        if(!s1.isEmpty())
+        x=s1.pop();
+        if(x==s2.peek())
+        s2.pop();
+        
     }
     
     public int top() {
-        if(!st.isEmpty())
-            return st.peek();
+        if(!s1.isEmpty())
+        return s1.peek();
         return -1;
+        
     }
     
     public int getMin() {
-        if(!minSt.isEmpty())
-            return minSt.peek();
+         if(!s2.isEmpty())
+        return s2.peek();
         return -1;
+        
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(value);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
