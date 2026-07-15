6457b4bd0e9220e6c9b95a625722e41fb5a28d14
jain-ishita-45 <jain.ishita.721@gmail.com>
2026-07-15 20:03:01 +0530

    Time: 8 ms (99.74%) | Memory: 59 MB (94.37%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -8,6 +8,16 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
 
     public ListNode findmiddle(ListNode head)
