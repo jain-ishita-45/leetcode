9d5c57526f290460bc5090ba453a622e852574a7
jain-ishita-45 <jain.ishita.721@gmail.com>
2026-07-25 08:26:30 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (16.54%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,32 +1,38 @@
+// import java.util.*;
 class Solution {
     public void rotate(int[][] matrix) {
-        int n=matrix.length;
-        int m=matrix[0].length;
-        for(int i=0;i<n;i++)
+       for(int i=0;i<matrix.length;i++) 
+       {
+        for(int j=0;j<matrix[0].length;j++)
         {
-            for(int j=0;j<m;j++)
-            {
-                if(i<=j)
-                {
-                    int temp=matrix[i][j];
-                    matrix[i][j]=matrix[j][i];
-                    matrix[j][i]=temp;
-                }
+            if(i<j)
+            {int temp=matrix[i][j];
+            matrix[i][j]=matrix[j][i];
+            matrix[j][i]=temp;
             }
+
         }
-        
-        for(int i=0;i<n;i++)
+       }
+    //    for(int i=0;i<matrix.length;i++)
+    //    {  Sytem.out.println(" ");
+
+    //     for(int j=0;j<matrix[0].length;j++)
+    //     {
+    //         Sytem.out.print(matrix[i][j]);
+    //     }
+    //    }
+       for(int i=0;i<matrix.length;i++)
+       {
+        int m=matrix[0].length-1;
+        for(int j=0;j<matrix[0].length/2;j++)
         {
-            m=matrix[0].length-1;
-            int j=0;
-            while(j<m)
-            {
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[i][m];
-                matrix[i][m]=temp;
-                j++;
-                m--;
-            }
+            
+            int temp=matrix[i][m];
+            matrix[i][m]=matrix[i][j];
+            matrix[i][j]=temp;
+            m--;
+
         }
+       }
     }
 }
