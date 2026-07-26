afa9e02dd69ee3cc0458cf91759167905781e7b3
jain-ishita-45 <jain.ishita.721@gmail.com>
2026-07-26 21:01:34 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (90.38%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,22 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
-        long N=n;
-        if (n<0)
+        if(n<0)
         {
             x=1/x;
             n=(-1)*n;
         }
-        return pow(x,n);
+        return powerr(x,n);
     }
 
-    public double pow(double x, int n)
+    public double powerr(double x, int n)
     {
-        if (n==0 )
-        return 1;
-        double half= pow(x,n/2);
+        if(n==0) return 1;
+        double ans=powerr(x,n/2);
         if(n%2==0)
-        return half*half;
-        else
-        return x*half*half;
+        return (double)ans*ans;
+        else return (double)ans*ans*x;
     }
 }
