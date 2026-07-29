f917534a415ccb33af761ec7a45a49e1c483893c
jain-ishita-45 <jain.ishita.721@gmail.com>
2026-07-29 07:12:09 +0530

    Time: 6 ms (85.57%) | Memory: 46.9 MB (10.96%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,57 +1,36 @@
-class Pair{
-    int first,second;
-    Pair(int first, int second)
-    {
-        this.first=first;
-        this.second=second;
-    }
-}
-
-class Tuple{
-    int first,second,third;
-    Tuple(int first, int second,int third)
-    {
-        this.first=first;
-        this.second=second;
-        this.third=third;
-    }
-}
-
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int dist[]=new int[n];
-        int c=0;
-        ArrayList<ArrayList<Pair>> adj=new ArrayList<>();
-        int m=flights.length;
+        ArrayList<ArrayList<int []>> adj=new ArrayList<>();
         for(int i=0;i<n;i++)
         adj.add(new ArrayList<>());
-        for(int i=0;i<m;i++)
-        adj.get(flights[i][0]).add(new Pair(flights[i][1],flights[i][2]));
-        Queue<Tuple> q=new LinkedList<>();
-        Arrays.fill(dist,(int)1e9);
+        for(int e[]:flights)
+        {
+            adj.get(e[0]).add(new int[]{e[1],e[2]});
+        }
+        int dist[]=new int[n];
+        Arrays.fill(dist,100000);
         dist[src]=0;
-        q.offer(new Tuple(0,src,0));
+        Queue<int []> q=new LinkedList<>();
+        q.offer(new int[]{0,src,0});
         while(!q.isEmpty())
         {
-            Tuple t=q.poll();
-            int stops=t.first;
-            int node=t.second;
-            int cost=t.third;
-            if(stops>k) continue;
-            for(Pair iter:adj.get(node))
+            int []row=q.poll();
+            int stop=row[0];
+            int node=row[1];
+            int cost=row[2];
+            if(stop>k) return (dist[dst]!=100000?dist[dst]:-1);
+            for(int []v:adj.get(node))
             {
-                int adjn=iter.first;
-                int dis=iter.second;
-                if(cost+dis<dist[adjn] && stops<=k)
+                int u=v[0],x=v[1];
+                if((cost+x)<dist[u] && stop<=k)
                 {
-                    q.add(new Tuple(stops+1,adjn,dis+cost));
-                    dist[adjn]=dis+cost;
+                    dist[u]=cost+x;
+                    q.offer(new int[]{stop+1,u,dist[u]});
                 }
-
-
             }
         }
-        if(dist[dst]==(int)1e9) return -1;
+        if(dist[dst]==100000) return -1;
         return dist[dst];
+        
     }
 }
