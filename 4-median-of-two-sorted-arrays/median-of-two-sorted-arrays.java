d17faa0f41042966121735013455cf733d0e545e
jain-ishita-45 <jain.ishita.721@gmail.com>
2026-08-05 08:05:09 +0530

    Time: 1 ms (100.00%) | Memory: 48.7 MB (72.41%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,28 +1,28 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if(nums2.length<nums1.length)
-        return findMedianSortedArrays(nums2,nums1);
-        int n1=nums1.length;
-        int n2=nums2.length;
-        int low=0,high=n1;
-        while(low<=high)
+        int n1=nums1.length,n2=nums2.length;
+        int n=n1+n2;
+        if(n1>n2) return findMedianSortedArrays(nums2,nums1);
+        int left=0,right=n1;
+        while(left<=right)
         {
-            int cut1=(low+high)/2;
-            int cut2=(n1+n2+1)/2-cut1;
-            int left1=cut1==0? Integer.MIN_VALUE:nums1[cut1-1];
-            int left2=cut2==0? Integer.MIN_VALUE:nums2[cut2-1];
-            int right1=cut1==n1? Integer.MAX_VALUE:nums1[cut1];
-            int right2=cut2==n2? Integer.MAX_VALUE:nums2[cut2];
-            if(left1<=right2 && left2<=right1)
+            int mid=(left+right)/2;
+            int mid2=(n1+n2+1)/2-mid;
+            int l1=mid==0?Integer.MIN_VALUE:nums1[mid-1];
+            int l2=mid2==0?Integer.MIN_VALUE:nums2[mid2-1];
+            int r1=mid==n1?Integer.MAX_VALUE:nums1[mid];
+            int r2=mid2==n2?Integer.MAX_VALUE:nums2[mid2];
+            if(l1<=r2 && l2<=r1)
             {
-                if((n1+n2)%2==0) 
-                return ( Math.max(left1,left2)+Math.min(right1,right2))/2.0;
+                if((n1+n2)%2==0)
+                return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
                 else
-                return Math.max(left1,left2);
+                return Math.max(l1,l2);
             }
-            else if(left1>=right2) high=cut1-1;
+            else if(l1>=r2 ) right=mid-1;
             else
-            low=cut1+1;
+            left=mid+1;
+
         }
         return 0.0;
     }
